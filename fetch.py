4be07a9b5421aefbc7c992476268d6e26c1bd34a
Markus Brown <markus04b@gmail.com>
2026-01-27 17:17:52 +0000

    initial edits

---
+++ fetch.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('baseball.db')
+cursor = conn.cursor()
+
+query = """
+    SELECT PlayerID, YearID, TeamID, HR
+    FROM batting
+    WHERE YearID = 1976 and teamID LIKE 'P%'
+    order by HR DESC
+"""
+
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+
+records_df = pd.DataFrame(records, columns=['PlayerID', 'YearID', 'TeamID', 'HR'])
+print(records_df)
