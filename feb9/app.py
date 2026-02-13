27f52afe408c5cec507be806ebbcb1cf789f3621
Markus Brown <markus04b@gmail.com>
2026-02-13 15:45:51 +0000

    friday changes

--- feb9/app.py
+++ feb9/app.py
@@ -4,15 +4,14 @@ import pandas as pd
 conn = sql.connect('../baseball.db')
 cursor = conn.cursor()
 query = """
-   select teamid, sum(hr) as seasonHR
-   from batting
-   where year_id = 2025
-   group by teamid
-   Having seasonHR >= 200
+   Select batting.yearid, name,batting.hr
+   from batting inner join teams
+   on batting.teamid = teams.teamid and batting.yearid = teams.yearid
+   where playerID = 'ruthba01'
 """
 cursor.execute(query)
 records = cursor.fetchall()
-records_df = pd.DataFrame(records, columns=['team', 'home_runs'])
+records_df = pd.DataFrame(records)
 conn.close()
 
 print(records_df)
